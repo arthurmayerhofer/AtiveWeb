ddb5d28b26df8ef1eeca46c020b35dcfc88a8ce7
arthurmayerhofer <mayerhofer.tec@gmail.com>
2024-04-10 05:38:39 -0300

    deploy

--- obj/Debug/net8.0/AtiveWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/AtiveWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtiveWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af76ba8320ce08087680894d4d7e12ebb7f302d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80765587786201e699a248d04d8d3459938da1c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtiveWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtiveWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
